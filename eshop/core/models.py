71ddaa6761f4163a881d946070cc7616a3c4e5af
Tom Peng <pengtom2012@gmail.com>
2016-03-21 23:27:56 -0400

    Added model: Category and Updated Product

--- eshop/core/models.py
+++ eshop/core/models.py
@@ -15,8 +15,6 @@ class Product(models.Model):
 	# zen-cart table products
 	# https://www.zen-cart.com/wiki/index.php/Table_products#Table_Details
 
-	product_id = models.AutoField(primary_key=True)
-
 	PRODUCT_STATUS_ACTIVE = 100
 	PRODUCT_STATUS_INACTIVE = 101
 	PRODUCT_STATUS_COMING_SOON = 102
@@ -27,26 +25,26 @@ class Product(models.Model):
 		(PRODUCT_STATUS_COMING_SOON, 'coming soon'),
 		(PRODUCT_STATUS_SOLD_OUT, 'sold out'),
 	)
-	product_status = models.IntegerField(choices=PRODUCT_STATUS_CHOICES, default=PRODUCT_STATUS_ACTIVE, db_index=True)
+	status = models.IntegerField(choices=PRODUCT_STATUS_CHOICES, default=PRODUCT_STATUS_ACTIVE, db_index=True)
 
-	product_date_added = models.DateTimeField(auto_now_add=True)
-	product_last_modified = models.DateTimeField(auto_now=True, blank=True, null=True)
-	product_date_available = models.DateTimeField(blank=True, null=True)
+	date_added = models.DateTimeField(auto_now_add=True)
+	last_modified = models.DateTimeField(auto_now=True, blank=True, null=True)
+	date_available = models.DateTimeField(blank=True, null=True)
 
 	# product_type =
-	product_quantity = models.FloatField(blank=True, null=True)
+	quantity = models.FloatField(blank=True, null=True)
 	# product_model =
-	product_image = models.ImageField(blank=True, upload_to='images/product_images/')
-	product_price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
-	product_is_virtual = models.BooleanField(default=False)
-	product_weight = models.DecimalField(blank=True, max_digits=10, decimal_places=2, null=True)
-	product_tax_class = models.BooleanField(default=True)  # Note: the actual tax is far more complicated than this
-	product_ordered_count = models.IntegerField(default=0, editable=False)
-	product_quantity_order_min = models.FloatField(blank=True, null=True)
-	product_quantity_order_units = models.FloatField(blank=True, null=True)
-	product_is_free = models.BooleanField(default=False)
-	product_quantity_order_max = models.FloatField(default=0)
-	product_sort_order = models.IntegerField(default=0)
+	image = models.ImageField(blank=True, upload_to='images/product_images/')
+	price = models.DecimalField(max_digits=10, decimal_places=4, default=0)
+	is_virtual = models.BooleanField(default=False)
+	weight = models.DecimalField(blank=True, max_digits=10, decimal_places=2, null=True)
+	tax_class = models.BooleanField(default=True)  # Note: the actual tax is far more complicated than this
+	ordered_count = models.IntegerField(default=0, editable=False)
+	quantity_order_min = models.FloatField(blank=True, null=True)
+	quantity_order_units = models.FloatField(blank=True, null=True)
+	is_free = models.BooleanField(default=False)
+	quantity_order_max = models.FloatField(default=0)
+	sort_order = models.IntegerField(default=0)
 
 	PRODUCT_DISCOUNT_TYPE_NONE = 200
 	PRODUCT_DISCOUNT_TYPE_PERCENTAGE = 201
@@ -58,20 +56,33 @@ class Product(models.Model):
 		(PRODUCT_DISCOUNT_TYPE_ACTUAL_PRICE, 'actual price'),
 		(PRODUCT_DISCOUNT_TYPE_AMOUNT_OFF_PRICE, 'amount off discount'),
 	)
-	PRODUCT_DISCOUNT_TYPE = models.IntegerField(choices=PRODUCT_DISCOUNT_TYPE_CHOICES, default=PRODUCT_DISCOUNT_TYPE_NONE)
+	discount_type = models.IntegerField(choices=PRODUCT_DISCOUNT_TYPE_CHOICES, default=PRODUCT_DISCOUNT_TYPE_NONE)
 
-	product_final_price = models.DecimalField(max_digits=10, decimal_places=4, default=0, db_index=True)
+	final_price = models.DecimalField(max_digits=10, decimal_places=4, default=0, db_index=True)
 
 	# zen-cart table products_description
 	# https://www.zen-cart.com/wiki/index.php/Table_products_description
 
-	product_name = models.CharField(max_length=64, db_index=True)
-	product_description = models.TextField(blank=True)
-	product_url = models.URLField(blank=True)
-	product_viewed_count = models.IntegerField(default=0, editable=False)
+	name = models.CharField(max_length=64, db_index=True)
+	description = models.TextField(blank=True)
+	url = models.URLField(blank=True)
+	viewed_count = models.IntegerField(default=0, editable=False)
 
 	# Foreign Keys?
 	# manufacturer_id =
 	# categories_id =
 	# language_id =
+
+
+class Category(models.Model):
+
+	# https://www.zen-cart.com/wiki/index.php/Table_categories
+
+	parent_id = models.ForeignKey('self', blank=True, null=True)
+	image = models.ImageField(blank=True, upload_to='images/category_images/')
+	sort_order = models.IntegerField(default=0)
+	date_added = models.DateTimeField(auto_now_add=True)
+	last_modified = models.DateTimeField(auto_now=True, blank=True, null=True)
+	status = models.BooleanField(default=1)
+
 
